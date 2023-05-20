fc8cbf6e85377152545182fcb797a118b108eb3a
numanrahi <arahi2994@gmail.com>
2023-05-20 12:09:41 +0600

    added custom api with mongodb

--- index.js
+++ index.js
@@ -9,10 +9,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-console.log(process.env.DB_PASS);
-console.log(process.env.DB_USER);
-
-const uri = `mongodb+srv://${process.env.USER}:${process.env.DB_PASS}@cluster0.vfido6v.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vfido6v.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -27,6 +24,22 @@ async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
+
+        const galleryCollectionOne = client.db("toyVerse").collection("gallaryOne");
+        const galleryCollectionTwo = client.db("toyVerse").collection("gallaryTwo");
+
+        app.get("/gallery-one", async (req, res) => {
+            const cursor = galleryCollectionOne.find();
+            const result = await cursor.toArray();
+            res.send(result);
+        })
+
+        app.get("/gallery-two", async (req, res) => {
+            const cursor = galleryCollectionTwo.find();
+            const result = await cursor.toArray();
+            res.send(result);
+        })
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
