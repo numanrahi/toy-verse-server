462cb2d7b93527154d0fde5def455a24e09e85c4
numanrahi <arahi2994@gmail.com>
2023-05-21 18:57:46 +0600

    single toy details api added

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
@@ -42,18 +42,34 @@ async function run() {
             res.send(result);
         })
 
-        app.get('/toy', async(req, res) => {
+        app.get('/toy', async (req, res) => {
             const cursor = toyCollection.find();
             const result = await cursor.toArray();
             res.send(result);
         })
 
-        app.post('/toy', async(req, res)=>{
+        app.post('/toy', async (req, res) => {
             const newToy = req.body;
             console.log(newToy);
             const result = await toyCollection.insertOne(newToy);
             res.send(result)
         })
+
+        app.get("/toy/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await toyCollection.findOne(query);
+            res.send(result);
+        })
+
+        /**
+         * app.get("/all-toys/:ID", async (req, res) => {
+            const id = req.params.ID;
+            const query = { _id: new ObjectId(id) }
+            const result = await allToyCollection.findOne(query);
+            res.send(result);
+        })
+         * */
 
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
